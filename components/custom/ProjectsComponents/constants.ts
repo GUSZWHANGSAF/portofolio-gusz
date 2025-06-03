b695b0631b2140c95795aa6700e6a7aed0e5b997
GUSZWHANGSAF <bagoszraisyah@gmail.com>
2025-06-03 21:11:51 +0700

    Add files via upload

---
+++ components/custom/ProjectsComponents/constants.ts
@@ -0,0 +1,53 @@
+import { Project } from "./types";
+
+export const projects: Project[] = [
+  {
+    title: "Web Wirid-Remaja",
+    src: "10.png",
+    color: "#ddd6fe",
+    url: "https://github.com/Arievf-R/SPPS",
+    role: "Fullstack Developer",
+  },
+  {
+    title: "Situ Coffie Shop",
+    src: "1.png",
+    color: "#dbeafe",
+    url: "https://mini-project-shop.vercel.app/",
+    role: "Front End Developer",
+  },
+  {
+    title: "Netflix-API",
+    src: "9.png",
+    color: "#fae8ff",
+    url: "https://netflixclone-woad-sigma.vercel.app/",
+    role: "Front End Developer",
+  },
+  {
+    title: " React User API",
+    src: "4.png",
+    color: "#ddd6fe",
+    url: "https://mini-project-ii-coral.vercel.app/",
+    role: "Front End Developer",
+  },
+  {
+    title: "Aplikasi Toko online",
+    src: "3.png",
+    color: "#fae8ff",
+    url: "https://github.com/Arievf-R/Computer-Store",
+    role: "Back End Developer",
+  },
+  {
+    title: "Arfrix Instagram API",
+    src: "11.png",
+    color: "#fae8ff",
+    url: "https://final-project-dibimbing-fawn.vercel.app/",
+    role: "Front End Developer",
+  },
+  {
+    title: "List of Users API",
+    src: "12.png",
+    color: "#fae8ff",
+    url: "https://listofuser.vercel.app/",
+    role: "Front End Developer",
+  },
+];
